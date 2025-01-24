0a75028b22bc718b16c03869a83d8ba16a537d60
qiuxan <qiuxan@gmail.com>
2025-01-24 22:34:49 +1100

    finished

--- src/assets/App.tsx
+++ src/assets/App.tsx
@@ -4,7 +4,20 @@ import ThreeLayout from './components/ThreeLayout'
 const App = () => {
   return (
     <div>
-        <ThreeLayout /> 
+      <ThreeLayout
+        left={<div style={{border:"2px solid #008c8c"}}>
+          <h4>Left aside</h4>
+        </div>}
+
+      right={<div style={{border:"2px solid #008c8c"}}>
+        <h4>right aside</h4>
+      </div>}
+      >
+        <div style={{border:"2px solid #f40"}}>
+          <h1>Content</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam veritatis rem officiis delectus itaque! Atque, earum quis iusto suscipit corrupti voluptate consectetur quo incidunt soluta, voluptas labore expedita dolore dolorem.</p>
+        </div>
+      </ThreeLayout> 
         
     </div>
   )
--- src/assets/components/ThreeLayout/index.tsx
+++ src/assets/components/ThreeLayout/index.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React from "react";
+import './index.css';
 
-const ThreeLayout = () => {
-  return (
-    <div>ThreeLayout</div>
-  
-  )
+interface ThreeLayoutProps {
+  leftWidth?: number;
+  rightWidth?: number;
+  minWidth?: number;
+  gap?: number;
+  children?: React.ReactNode;
+  left?: React.ReactNode;
+  right?: React.ReactNode;
 }
 
-export default ThreeLayout
+
+const ThreeLayout = (props: ThreeLayoutProps) => {
+
+  const defaultProps = {
+    leftWidth:200,
+    rightWidth:200,
+    minWidth:800,
+    gap:2
+  };
+
+  const data = Object.assign({}, defaultProps, props);
+
+  return (
+    <div className="three-layout" style={{minWidth : data.minWidth}}>
+      <div className="main">
+        {props.children}
+      </div>
+      <div className="aside-left" style={{width:data.leftWidth, marginRight:data.gap}}>
+        {props.left}
+      </div>
+      <div className="aside-right" style={{width:data.rightWidth, marginLeft:data.gap}}>
+        {props.right}
+      </div>
+    </div>
+  );
+};
+
+export default ThreeLayout;
+ 
