d8ce105cddb64c28f9223e3b1e4664420b4b0e15
qiuxan <qiuxan@gmail.com>
2025-01-24 22:05:47 +1100

    first commit

---
+++ src/assets/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ThreeLayout from './components/ThreeLayout'
+
+const App = () => {
+  return (
+    <div>
+        <ThreeLayout /> 
+        
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/components/ThreeLayout/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ThreeLayout = () => {
+  return (
+    <div>ThreeLayout</div>
+  
+  )
+}
+
+export default ThreeLayout
